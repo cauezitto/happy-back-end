1c2361cc8f7c8acea0498ab38d2c14f8a1e26c50
cauezitto <caue.matheus.granrodeo@gmail>
2020-10-29 12:48:04 -0300

    resetando base de dados

--- src/views/imagesView.ts
+++ src/views/imagesView.ts
@@ -4,7 +4,7 @@ export default {
 
     render(image: Image){
         return{
-            id: image.id,
+            id: image.id, 
             url: `https://stonks-test-api.herokuapp.com/uploads/${image.path}`
         }
     },
